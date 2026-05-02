c6174ad5f4252dd9084d632a120231adae028ec1
Nahid <jr6259094@gmail.com>
2026-05-02 14:35:38 +0600

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-from src.routes.ai_router import router
+from src.routes.ai_router import router as aiRouter
+from src.routes.userRoute import router as UserRouter
 
 # cool and clean
 app = FastAPI(title="Ai learning")
-app.include_router(router)
+
+routes = [aiRouter,UserRouter]
+
+for r in routes:
+    app.include_router(r)
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
