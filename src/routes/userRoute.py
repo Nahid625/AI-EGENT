26b36a9b23a24f8f2920ef44256ea79cb9308255
Nahid <jr6259094@gmail.com>
2026-05-02 18:49:11 +0600

    login router has been created

--- src/routes/userRoute.py
+++ src/routes/userRoute.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from src.controller.auth import signup
-from src.models.models import SignupResponse, UserRequest # Import your request model
+from src.controller.auth import login, signup
+from src.models.models import LoginRequest, SignupResponse, UserRequest # Import your request model
 from src.config.db import get_db # Import your DB session logic
 
 router = APIRouter(
@@ -13,3 +13,8 @@ router = APIRouter(
 def sign_up_route_function(user_data: UserRequest, db: Session = Depends(get_db)):
     # You need to pass the user_data AND the db session to your controller
     return signup(user_data=user_data, db=db)
+
+
+@router.post("/login",response_model=SignupResponse)
+def login_Router_Function(user_data : LoginRequest ,db : Session = Depends(get_db)):
+    return login(user_data,db)
