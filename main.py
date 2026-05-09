74b371af04fdbb7190fe64574cd12608dd86b317
Nahid <jr6259094@gmail.com>
2026-05-09 15:49:23 +0600

    fix url 8000 to 8080

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ for r in routes:
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
